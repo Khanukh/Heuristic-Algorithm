5ab1874ff77926ef73731d9ab4f6e5a280f6ec43
Emad Shahbazi <106076729+Khanukh@users.noreply.github.com>
2023-01-06 18:06:01 +0330

    Add files via upload

---
+++ wsn_200_35/gwo updated/initialization.m
@@ -0,0 +1,40 @@
+%___________________________________________________________________%
+%  Grey Wolf Optimizer (GWO) source codes version 1.0               %
+%                                                                   %
+%  Developed in MATLAB R2011b(7.13)                                 %
+%                                                                   %
+%  Author and programmer: Seyedali Mirjalili                        %
+%                                                                   %
+%         e-Mail: ali.mirjalili@gmail.com                           %
+%                 seyedali.mirjalili@griffithuni.edu.au             %
+%                                                                   %
+%       Homepage: http://www.alimirjalili.com                       %
+%                                                                   %
+%   Main paper: S. Mirjalili, S. M. Mirjalili, A. Lewis             %
+%               Grey Wolf Optimizer, Advances in Engineering        %
+%               Software , in press,                                %
+%               DOI: 10.1016/j.advengsoft.2013.12.007               %
+%                                                                   %
+%___________________________________________________________________%
+
+% This function initialize the first population of search agents
+function Positions=initialization(SearchAgents_no,dim,ub,lb)
+
+Boundary_no= size(ub,2); % numnber of boundaries
+
+% If the boundaries of all variables are equal and user enter a signle
+% number for both ub and lb
+if Boundary_no==1
+     Positions1 = randi(2,[ceil(3*dim/4) dim])+2;
+     Positions2 = (randi(2,[floor(dim/4) dim])-1)*3+1;
+     Positions=[Positions1 ; Positions2];
+end
+
+% If each variable has a different lb and ub
+if Boundary_no>1
+    for i=1:dim
+        ub_i=ub(i);
+        lb_i=lb(i);
+        Positions(:,i)=rand(SearchAgents_no,1).*(ub_i-lb_i)+lb_i;
+    end
+end
---
+++ wsn_200_35/mixed gwo and woa/WOA.m
@@ -0,0 +1,192 @@
+%_________________________________________________________________________%
+%  Whale Optimization Algorithm (WOA) source codes demo 1.0               %
+%                                                                         %
+%  Developed in MATLAB R2011b(7.13)                                       %
+%                                                                         %
+%  Author and programmer: Seyedali Mirjalili                              %
+%                                                                         %
+%         e-Mail: ali.mirjalili@gmail.com                                 %
+%                 seyedali.mirjalili@griffithuni.edu.au                   %
+%                                                                         %
+%       Homepage: http://www.alimirjalili.com                             %
+%                                                                         %
+%   Main paper: S. Mirjalili, A. Lewis                                    %
+%               The Whale Optimization Algorithm,                         %
+%               Advances in Engineering Software , in press,              %
+%               DOI: http://dx.doi.org/10.1016/j.advengsoft.2016.01.008   %
+%                                                                         %
+%_________________________________________________________________________%
+
+
+% The Whale Optimization Algorithm
+function [Leader_score,Leader_pos,Convergence_curve]=WOA(SearchAgents_no,Max_iter,lb,ub,dim,fobj)
+
+% initialize position vector and score for the leader
+Leader_pos=zeros(1,dim);
+Leader_score=inf; %change this to -inf for maximization problems
+
+%wolf initialization 
+Alpha_pos=zeros(1,dim);
+Alpha_score=inf; %change this to -inf for maximization problems
+
+Beta_pos=zeros(1,dim);
+Beta_score=inf; %change this to -inf for maximization problems
+
+Delta_pos=zeros(1,dim);
+Delta_score=inf; %change this to -inf for maximization problems
+
+%Initialize the positions of search agents
+Positions=initialization(SearchAgents_no,dim,ub,lb);
+
+Convergence_curve=zeros(1,Max_iter);
+
+t=0;% Loop counter
+l = 0;
+% Main loop
+while t<Max_iter 
+    for i=1:size(Positions,1)
+        
+        % Return back the search agents that go beyond the boundaries of the search space
+        Flag4ub=Positions(i,:)>ub;
+        Flag4lb=Positions(i,:)<lb;
+        Positions(i,:)=(Positions(i,:).*(~(Flag4ub+Flag4lb)))+ub.*Flag4ub+lb.*Flag4lb;
+        Positions(i,:) = round(Positions(i,:));
+        % Calculate objective function for each search agent
+        fitness=fobj((Positions(i,:)));
+       
+        %%
+        % Update the leader
+        if fitness<Leader_score % Change this to > for maximization problem
+            Leader_score=fitness; % Update alpha
+            Leader_pos=Positions(i,:);
+        end
+         %% the breaking rule;
+        if fitness < 150 
+            
+            break;
+        end
+    end
+    
+    a=2-t*((2)/Max_iter); % a decreases linearly fron 2 to 0 in Eq. (2.3)
+    
+    % a2 linearly dicreases from -1 to -2 to calculate t in Eq. (3.12)
+    a2=-1+t*((-1)/Max_iter);
+    
+    % Update the Position of search agents 
+    for i=1:size(Positions,1)
+        r1=rand(); % r1 is a random number in [0,1]
+        r2=rand(); % r2 is a random number in [0,1]
+        
+        A=2*a*r1-a;  % Eq. (2.3) in the paper
+        C=2*r2;      % Eq. (2.4) in the paper
+        
+        
+        b=1;               %  parameters in Eq. (2.5)
+        l=(a2-1)*rand+1;   %  parameters in Eq. (2.5)
+        
+        p = rand();        % p in Eq. (2.6)
+        
+        for j=1:size(Positions,2)
+            
+            if p<0.5   
+                if abs(A)>=1
+                    rand_leader_index = floor(SearchAgents_no*rand()+1);
+                  
+                    X_rand = Positions(rand_leader_index, :);
+                    D_X_rand=abs(C*X_rand(j)-Positions(i,j)); % Eq. (2.7)
+                    Positions(i,j)=X_rand(j)-A*D_X_rand;      % Eq. (2.8)
+                    
+                elseif abs(A)<1
+                    D_Leader=abs(C*Leader_pos(j)-Positions(i,j)); % Eq. (2.1)
+                    Positions(i,j)=Leader_pos(j)-A*D_Leader;      % Eq. (2.2)
+                end
+                
+            elseif p>=0.5
+              
+                distance2Leader=abs(Leader_pos(j)-Positions(i,j));
+                % Eq. (2.5)
+                Positions(i,j)=distance2Leader*exp(b.*l).*cos(l.*2*pi)+Leader_pos(j);
+                
+            end
+            
+        end
+    end
+    Positions = round(Positions);
+   
+   % [t Leader_score]
+%    display( " whale positions " + Positions)
+   %% gray wolf
+   for i=1:size(Positions,1)  
+        
+       % Return back the search agents that go beyond the boundaries of the search space
+        Flag4ub=Positions(i,:)>ub;
+        Flag4lb=Positions(i,:)<lb;
+        Positions(i,:)=(Positions(i,:).*(~(Flag4ub+Flag4lb)))+ub.*Flag4ub+lb.*Flag4lb;               
+        
+        % Calculate objective function for each search agent
+        fitness=fobj((Positions(i,:)));
+        
+        % Update Alpha, Beta, and Delta
+        if fitness<Alpha_score 
+            Alpha_score=fitness; % Update alpha
+            Alpha_pos=Positions(i,:);
+        end
+        
+        if fitness>Alpha_score && fitness<Beta_score 
+            Beta_score=fitness; % Update beta
+            Beta_pos=Positions(i,:);
+        end
+        
+        if fitness>Alpha_score && fitness>Beta_score && fitness<Delta_score 
+            Delta_score=fitness; % Update delta
+            Delta_pos=Positions(i,:);
+        end
+    end
+    
+    
+    a=2-l*((2)/Max_iter); % a decreases linearly fron 2 to 0
+    
+    % Update the Position of search agents including omegas
+    for i=1:size(Positions,1)
+        for j=1:size(Positions,2)     
+                       
+            r1=rand(); % r1 is a random number in [0,1]
+            r2=rand(); % r2 is a random number in [0,1]
+            
+            A1=2*a*r1-a; % Equation (3.3)
+            C1=2*r2; % Equation (3.4)
+            
+            D_alpha=abs(C1*Alpha_pos(j)-Positions(i,j)); % Equation (3.5)-part 1
+            X1=Alpha_pos(j)-A1*D_alpha; % Equation (3.6)-part 1
+                       
+            r1=rand();
+            r2=rand();
+            
+            A2=2*a*r1-a; % Equation (3.3)
+            C2=2*r2; % Equation (3.4)
+            
+            D_beta=abs(C2*Beta_pos(j)-Positions(i,j)); % Equation (3.5)-part 2
+            X2=Beta_pos(j)-A2*D_beta; % Equation (3.6)-part 2       
+            
+            r1=rand();
+            r2=rand(); 
+            
+            A3=2*a*r1-a; % Equation (3.3)
+            C3=2*r2; % Equation (3.4)
+            
+            D_delta=abs(C3*Delta_pos(j)-Positions(i,j)); % Equation (3.5)-part 3
+            X3=Delta_pos(j)-A3*D_delta; % Equation (3.5)-part 3             
+            
+            Positions(i,j)= round((X1+X2+X3)/3);% Equation (3.7)
+%             display( " wolf positions " + Positions)
+        end
+    end
+  
+     t=t+1;
+     l = l+1;
+    Convergence_curve(t)=Leader_score;
+   
+end
+
+
+
